69635f9d246ebf2b282c2873db61b130e93fd4c8
himdyoti <himdyoti@localhost.localdomain>
2018-04-22 18:55:45 +0530

    for user defined arg
    when user defined arg is provided

--- decoratorChain.py
+++ decoratorChain.py
@@ -1,13 +1,19 @@
+
 def make_html(htag):
     def deco(fn):
         def wrapper(*arg,**kargs):
-            return '<{}>{}</{}>'.format(htag,fn(),htag)
+            txtt = kargs.get('text')
+            if txtt:
+                return '<{}>{}</{}>'.format(htag,fn(text=txtt),htag)
+            else:
+                return '<{}>{}</{}>'.format(htag,fn(),htag)
         return wrapper
     return deco
 
 @make_html('div') 
-@make_html('p')
-@make_html('strong')
+@make_html('p')        #make_html('p')('<strong>I code with PyBites</strong>')
+@make_html('strong')   # make_html('strong')(get_text)
 def get_text(text='I code with PyBites'):
     return text
-print(get_text())  
+print(get_text())
+
